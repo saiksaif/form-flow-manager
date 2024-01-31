5a64e43acac18beb65d34c0a59675e1be2f092ca
Saif Ali <116622210+saiksaif@users.noreply.github.com>
2024-01-31 13:23:11 +0500

    Ui fixes

--- src/Components/items/addInput.jsx
+++ src/Components/items/addInput.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AddInput = ({data, update}) => {
   return (
     <div>
-        <button>Add Input</button>
+        <button className='border rounded bg-gray-300 min-w-[150px]'>Add Input</button>
         
         {/* Insert Modal here and relevent functions/fields inside the modal */}
     </div>
